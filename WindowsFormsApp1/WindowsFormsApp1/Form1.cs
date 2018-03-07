0736dadb2b1614107730647f4e4b9b330946dad1
Li-Hsuan <vivian.wu@gooee.com>
2018-03-07 13:50:27 +0800

    incourrect

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            labelName.Text = "Hello C#.";
+            labelName.Text = "Hello C#."
         }
     }
 }
